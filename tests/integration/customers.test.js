b64b0646ff0216d15066a477cb438a6f59960d0a
SirPhemmiey <oluwafemiakinde@gmail.com>
2019-03-10 13:54:30 +0100

    Add tests

---
+++ tests/integration/customers.test.js
@@ -0,0 +1,17 @@
+/***
+ * Test all customer endpoints
+ */
+
+"use strict";
+
+const request = require("supertest");
+const app = require("../../server");
+const { expect } = require("chai");
+
+describe("Customer Operations", () => {
+  
+    describe("", () => {
+      
+    })
+    
+})
